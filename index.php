7dd0acbea377243515ba4380bdc40f94eaddf304
Andy Brody <andy@abrody.com>
2012-09-08 20:30:40 -0400

    Add google analytics script.

--- index.php
+++ index.php
@@ -15,6 +15,18 @@ function html_response() {
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-20805386-1']);
+      _gaq.push(['_setDomainName', 'none']);
+      _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 </head>
 <body lang=en>
     <!-- User Agent: "<?= $_SERVER['HTTP_USER_AGENT'] ?>" -->
